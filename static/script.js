8785d573686032a064774fc2c7c8b38dbc9d8c17
Nathalie Taylor <nmtzampieri@gmail.com>
2024-09-08 17:04:30 -0300

    habilidades + responsividade e ancoragem

--- static/script.js
+++ static/script.js
@@ -10,3 +10,16 @@ menuMobile.addEventListener('click',() =>{
     body.classList.toggle("menu-nav-active");
 
 });
+
+/* fecha meno quando clicar em algum item e muda o icone para list */
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click",() => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x","bi-list");
+        }
+    })
+})
